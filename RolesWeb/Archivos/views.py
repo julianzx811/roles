f8610270c9f6920f242acaf5b57a0bba047f952f
yulian david <julianolomanolox@hotmail.com>
2023-09-08 18:53:27 -0500

    uploading excel file working

--- RolesWeb/Archivos/views.py
+++ RolesWeb/Archivos/views.py
@@ -1,6 +1,41 @@
+import openpyxl
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if "GET" == request.method:
+        return render(request, "Archivos/index.html", {})
+    else:
+        excel_file = request.FILES["excel_file"]
+
+        # you may put validations here to check extension or file size
+
+        wb = openpyxl.load_workbook(excel_file)
+
+        # getting all sheets
+        sheets = wb.sheetnames
+        print(sheets)
+
+        # getting a particular sheet
+        worksheet = wb["Sheet1"]
+        print(worksheet)
+
+        # getting active sheet
+        active_sheet = wb.active
+        print(active_sheet)
+
+        # reading a cell
+        print(worksheet["A1"].value)
+
+        excel_data = list()
+        # iterating over the rows and
+        # getting value from each cell in row
+        for row in worksheet.iter_rows():
+            row_data = list()
+            for cell in row:
+                row_data.append(str(cell.value))
+                print(cell.value)
+            excel_data.append(row_data)
+
+        return render(request, "Archivos/index.html", {"excel_data": excel_data})
--- RolesWeb/RolesWeb/urls.py
+++ RolesWeb/RolesWeb/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("Archivos/", include("Archivos.urls")),
+    path("", include("Archivos.urls")),
     path("admin/", admin.site.urls),
 ]
