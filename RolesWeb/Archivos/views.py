99e75758a6fe257a28813fb9d1eee0fb7a65d6f3
yulian david <julianolomanolox@hotmail.com>
2023-09-11 23:11:11 -0500

    try catch implementado

--- RolesWeb/Archivos/views.py
+++ RolesWeb/Archivos/views.py
@@ -8,43 +8,48 @@ def index(request):
     if "GET" == request.method:
         return render(request, "Archivos/index.html", {})
     else:
-        excel_file = request.FILES["excel_file"]
-        periodo = request.POST.get('file_type')
-        print("periodo",periodo)
-        # you may put validations here to check extension or file size
+        try:
+            excel_file = request.FILES["excel_file"]
+            periodo = request.POST.get('file_type')
+            print("periodo",periodo)
+            # you may put validations here to check extension or file size
 
-        wb = openpyxl.load_workbook(excel_file)
+            wb = openpyxl.load_workbook(excel_file)
 
-        # getting all sheets
-        sheets = wb.sheetnames
-        print(sheets)
+            # getting all sheets
+            sheets = wb.sheetnames
+            print(sheets)
 
-        # getting a particular sheet
-        worksheet = wb["Sheet1"]
-        print(worksheet)
+            # getting a particular sheet
+            worksheet = wb["Sheet1"]
+            print(worksheet)
 
-        # getting active sheet
-        active_sheet = wb.active
-        print(active_sheet)
+            # getting active sheet
+            active_sheet = wb.active
+            print(active_sheet)
 
-        # reading a cell
-        print(worksheet["C1"].value)
+            # reading a cell
+            print(worksheet["C1"].value)
 
-        excel_data = []
-        # iterating over the rows and
-        # getting value from each cell in row
-        for row in worksheet.iter_rows(min_row=14, max_row=59, max_col=8):
-            row_data = []
-            for cell in row:
-                if(str(cell.value)!='None'):
-                    row_data.append(str(cell.value))
-                    print(cell.value)
-            if len(row_data) > 1:
-                excel_data.append(row_data)
-        print(excel_data)
-        excel_data
-        for datos in excel_data:
-            est1 = Estudiante(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5], datos[6],periodo)
-            est1.save()
+            excel_data = []
+            # iterating over the rows and
+            # getting value from each cell in row
+            for row in worksheet.iter_rows(min_row=14, max_row=59, max_col=8):
+                row_data = []
+                for cell in row:
+                    if(str(cell.value)!='None'):
+                        row_data.append(str(cell.value))
+                        print(cell.value)
+                if len(row_data) > 1:
+                    excel_data.append(row_data)
+            print(excel_data)
+            excel_data
+            for datos in excel_data:
+                est1 = Estudiante(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5], datos[6],periodo)
+                est1.save()
+            return render(request, "Archivos/index.html", {"excel_data": excel_data})
+        except  Exception as err :
+            return render(request, "Archivos/index.html", {"error": err})
 
-        return render(request, "Archivos/index.html", {"excel_data": excel_data})
+
+        
