b5675bfa881c6c97ba6e2594855baf984a85d40d
Cristian Giraldo <cristian_gira@hotmail.com>
2023-09-17 19:19:12 -0500

    Conexión
    Se conecta con  PostgresSQL
    NAME: practias

--- RolesWeb/RolesWeb/settings.py
+++ RolesWeb/RolesWeb/settings.py
@@ -79,11 +79,16 @@ WSGI_APPLICATION = "RolesWeb.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'practicas',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
