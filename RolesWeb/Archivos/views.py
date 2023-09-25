9ea1ce22e5dab931fedd6cf1165f95c994fa3120
Cristian Giraldo <cristian_gira@hotmail.com>
2023-09-24 20:21:47 -0500

    Aspirante
    CREO que sube archivo aspirante

--- RolesWeb/Archivos/views.py
+++ RolesWeb/Archivos/views.py
@@ -2,8 +2,7 @@ import openpyxl
 import psycopg2
 from django.http import HttpResponse
 from django.shortcuts import render
-
-from .models import Estudiante
+from .models import Estudiante, Aspirantes, Contrato, Estado_Practica
 
 
 def index(request):
@@ -55,58 +54,75 @@ def cargarArchivoEstudiantes(request):
             )
 
 def cargarArchivoEstudiantesDos(request):
-    if "GET" == request.method:
-        return render(request, "Archivos/CargaEstudiantesDos.html", {})
+    if request.method == "GET":
+        return render(request, "Archivos/cargaEstudiantesDos.html", {})
     else:
         try:
             excel_file = request.FILES["excel_file"]
-            periodo = request.POST.get("file_type")
-
-            # print("periodo",periodo)
-            # you may put validations here to check extension or file size
-
             wb = openpyxl.load_workbook(excel_file)
-
-            # getting all sheets
-            sheets = wb.sheetnames
-            # print(sheets)
-
-            # getting a particular sheet
-            worksheet = wb["Sheet1"]
-            # print(worksheet)
-
-            # getting active sheet
-            active_sheet = wb.active
-            # print(active_sheet)
-
-            # reading a cell
-            print(worksheet["C1"].value)
+            worksheet = wb.active  # Utiliza la hoja activa o ajusta según la hoja que necesites
 
             excel_data = []
-            # iterating over the rows and
-            # getting value from each cell in row
-            for row in worksheet.iter_rows(min_row=14, max_col=8):
-                print("Impresion error",row)
-                row_data = []
-                for cell in row:
-                    if str(cell.value) != "None":
-                        row_data.append(str(cell.value))
-                        print(cell.value)
-                if len(row_data) > 1:
-                    excel_data.append(row_data)
-            excel_data
-            for datos in excel_data:
-                est1 = Estudiante(
-                    datos[0],
-                    datos[1],
-                    datos[2],
-                    datos[3],
-                    datos[4],
-                    datos[5],
-                    datos[6],
-                    periodo,
-                )
-                est1.save()
+
+            for row in worksheet.iter_rows(values_only=True):
+                row_data = list(row)
+                excel_data.append(row_data)
+
+            for row in excel_data:
+                # Verifica si la fila tiene suficientes elementos para procesar
+                if len(row) >= 28:  # Ajusta el número según la cantidad de columnas en tu Excel
+                    # Crear un objeto Estudiante
+                    estudiante = Estudiante(
+                        codigo=row[0],
+                        programa=row[1],
+                        email_institucional=row[2],
+                        email_personal=row[3],
+                        telefono=row[4],
+                        nombre=row[5],
+                        apellidos=row[6],
+                        cedula=row[7],
+                        celular=row[8],
+                        periodo_lectivo=row[9],
+                        # Ajusta esto para el campo plan_estudios
+                    )
+                    estudiante.save()
+
+                    # Crear un objeto Aspirantes relacionado con el estudiante
+                    aspirantes = Aspirantes(
+                        periodo_practica=row[10],
+                        aprobación_Programa=row[11],
+                        matriculado_Academica_y_Financieramente=row[12],
+                        inscripcion=row[13],
+                        curso_induccion_y_rl=row[14],
+                        ruta_preparacion_vida_laboral=row[15],
+                        envio_hv=row[16],
+                        titulo_tecnico_o_tecnologo=row[17],
+                        codigo_estudiante=estudiante,
+                    )
+                    aspirantes.save()
+
+                    # Crear un objeto Contrato relacionado con el estudiante
+                    contrato = Contrato(
+                        tipo_Contrato=row[18],
+                        fecha_Inicio=row[19],
+                        fecha_Final=row[20],
+                        encargado_Proceso_Seleccion=row[21],
+                        datos_Tutor_O_Jefe_Directivo=row[22],
+                        documentos_Pendientes=row[23],
+                        sector=row[24],
+                    )
+                    contrato.save()
+
+                    # Crear un objeto Estado_Practica relacionado con el estudiante, Aspirantes y Contrato
+                    estado_practica = Estado_Practica(
+                        codigo_estudiante=estudiante,
+                        practica_Donde_Labora_EmpresaFliar_Emprendim_Otro=row[25],
+                        estado_ubicación=row[26],
+                        comentarios=row[27],
+                        item=aspirantes,
+                        id_contrato=contrato,
+                    )
+                    estado_practica.save()
 
             return render(
                 request, "Archivos/cargaEstudiantesDos.html", {"excel_data": excel_data}
@@ -114,5 +130,5 @@ def cargarArchivoEstudiantesDos(request):
         except Exception as error:
             print(error)
             return render(
-                request, "Archivos/CargaEstudiantesDos.html", {"error": str(error)}
+                request, "Archivos/cargaEstudiantesDos.html", {"error": str(error)}
             )
