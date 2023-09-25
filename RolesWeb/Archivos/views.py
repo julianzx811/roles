d4a2dc61ee9977375fe20b7f3f2a75d0957c7679
SamMartSan <samymartsa123@gmail.com>
2023-09-24 23:05:47 -0500

    actualizadas las variables a subir

--- RolesWeb/Archivos/views.py
+++ RolesWeb/Archivos/views.py
@@ -2,7 +2,7 @@ import openpyxl
 import psycopg2
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Estudiante, Aspirantes, Contrato, Estado_Practica
+from .models import Estudiante, Aspirantes, Contrato, Estado_Practica, Plan_estudios
 
 
 def index(request):
@@ -76,26 +76,30 @@ def cargarArchivoEstudiantesDos(request):
                 # Verifica si la fila tiene suficientes elementos para procesar
                 if len(row) >= 28:  # Ajusta el número según la cantidad de columnas en tu Excel
                     # Crear un objeto Estudiante
+                    print(row)
+                    plan_estudios = Plan_estudios(
+                        jornada=row[12]
+                    )
+                    plan_estudios.save()
                     estudiante = Estudiante(
-                        codigo=row[0],
-                        programa=row[1],
-                        email_institucional=row[2],
-                        email_personal=row[3],
-                        telefono=row[4],
+                        programa=row[11],
+                        codigo=row[7],
+                        email_institucional=row[10],
+                        email_personal=row[10],
+                        telefono=row[9],
                         nombre=row[5],
                         apellidos=row[6],
-                        cedula=row[7],
-                        celular=row[8],
-                        periodo_lectivo=row[9],
+                        cedula=row[8],
+                        celular=row[9],
                         # Ajusta esto para el campo plan_estudios
                     )
                     estudiante.save()
 
                     # Crear un objeto Aspirantes relacionado con el estudiante
                     aspirantes = Aspirantes(
-                        periodo_practica=row[10],
-                        aprobación_Programa=row[11],
-                        matriculado_Academica_y_Financieramente=row[12],
+                        periodo_practica=row[1],
+                        aprobación_Programa=row[2],
+                        matriculado_Academica_y_Financieramente=row[4],
                         inscripcion=row[13],
                         curso_induccion_y_rl=row[14],
                         ruta_preparacion_vida_laboral=row[15],
@@ -107,26 +111,27 @@ def cargarArchivoEstudiantesDos(request):
 
                     # Crear un objeto Contrato relacionado con el estudiante
                     contrato = Contrato(
-                        tipo_Contrato=row[18],
-                        fecha_Inicio=row[19],
-                        fecha_Final=row[20],
-                        encargado_Proceso_Seleccion=row[21],
-                        datos_Tutor_O_Jefe_Directivo=row[22],
-                        documentos_Pendientes=row[23],
-                        sector=row[24],
+                        tipo_Contrato=row[21],
+                        fecha_Inicio=row[22],
+                        fecha_Final=row[23],
+                        encargado_Proceso_Seleccion=row[24],
+                        datos_Tutor_O_Jefe_Directivo=row[25],
+                        documentos_Pendientes=row[26],
+                        sector=row[27],
                     )
                     contrato.save()
 
                     # Crear un objeto Estado_Practica relacionado con el estudiante, Aspirantes y Contrato
                     estado_practica = Estado_Practica(
                         codigo_estudiante=estudiante,
-                        practica_Donde_Labora_EmpresaFliar_Emprendim_Otro=row[25],
-                        estado_ubicación=row[26],
-                        comentarios=row[27],
+                        practica_Donde_Labora_EmpresaFliar_Emprendim_Otro=row[18],
+                        estado_ubicación=row[19],
+                        comentarios=row[20],
                         item=aspirantes,
                         id_contrato=contrato,
                     )
                     estado_practica.save()
+                    
 
             return render(
                 request, "Archivos/cargaEstudiantesDos.html", {"excel_data": excel_data}
