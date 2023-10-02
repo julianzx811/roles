3d71d3b6a9ac655e303506ee73b84db8d153d7e9
Cristian Giraldo <cristian_gira@hotmail.com>
2023-10-01 23:52:36 -0500

    Errores
    Error en URLS.py

--- RolesWeb/Archivos/urls.py
+++ RolesWeb/Archivos/urls.py
@@ -5,14 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("cargaEstudiantes/", views.cargarArchivoEstudiantes, name="cargaEstudiantes"),
-    path(
-        "cargaEstudiantesDos/",
-        views.cargarArchivoEstudiantesDos,
-        name="cargaEstudiantesDos",
-    ),
-    path(
-        "CrearMonitor/",
-        views.CrearMonitor,
-        name="CrearMonitor",
-    ),
+    path("cargaEstudiantesDos/",views.cargarArchivoEstudiantesDos,name="cargaEstudiantesDos"),
+    path("CrearMonitor/",views.CrearMonitor,name="CrearMonitor"),
 ]
