9ad81ef54092592ff5ca0bac23bb9e10977d9efc
yulian david <julianolomanolox@hotmail.com>
2023-11-08 09:41:21 -0500

    apunto de meter el actulizar archivo

--- RolesWeb/RolesWeb/settings.py
+++ RolesWeb/RolesWeb/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = "RolesWeb.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'practicas',
-        'USER': 'postgres',
-        'PASSWORD': '24713592',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "practicas",
+        "USER": "postgres",
+        "PASSWORD": "7878",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
