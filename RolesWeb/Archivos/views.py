5179373aa4c326e667f94cde25ffdee05f850aba
SamMartSan <samymartsa123@gmail.com>
2023-11-15 10:27:21 -0500

    Todo lo mio listo

--- RolesWeb/Archivos/views.py
+++ RolesWeb/Archivos/views.py
@@ -191,6 +191,13 @@ def indexOficinaPracticas(request):
         {"archivo_subido": archivo_subido, "existe": existe},
     )
 
+def indexAuxiliarOficinaPracticas(request):
+    existe = Estudiante.objects.exists()
+    return render(
+        request,
+        "Archivos/vistaAuxiliarOficinaPracticas.html",
+        {"archivo_subido": archivo_subido, "existe": existe},
+    )
 
 def indexDocenteMonitor(request):
     existe = Estudiante.objects.exists()
@@ -225,6 +232,76 @@ def indexAdministrador(request):
         },
     )
 
+
+def agregarNuevoLider(request):
+    if request.method == "GET":
+        return render(request, "Archivos/CrearLiderOficina.html")
+    else:
+        creaLiderOficina = False
+        liderExiste = False
+        
+        usuario = request.POST["correo"].split("@")[0]
+       
+        if (
+            Perfiles.objects.filter(usuario=usuario).exists()
+            == False
+        ):
+            Nombre = request.POST["nombre"]
+            Codigo = request.POST["codigo"]
+            Correo = request.POST["correo"]
+            perfil = Perfiles(
+                usuario=Correo.split("@")[0],
+                contrasena=Codigo,
+                nombre=Nombre,
+                cargo="Lider Oficina de Practicas",
+            )
+            perfil.save()
+            creaLiderOficina = True
+        else:
+            liderExiste = True
+        return render(
+            request,
+             "Archivos/CrearLiderOficina.html",
+            {
+                "creaLiderOficina": creaLiderOficina,
+                "liderExiste": liderExiste
+            },
+        )
+
+def agregarNuevoAuxiliar(request):
+    if request.method == "GET":
+        return render(request, "Archivos/CrearAuxiliarOficina.html")
+    else:
+        creaAuxiliarOficina = False
+        auxiliarExiste = False
+        
+        usuario = request.POST["correo"].split("@")[0]
+       
+        if (
+            Perfiles.objects.filter(usuario=usuario).exists()
+            == False
+        ):
+            Nombre = request.POST["nombre"]
+            Codigo = request.POST["codigo"]
+            Correo = request.POST["correo"]
+            perfil = Perfiles(
+                usuario=Correo.split("@")[0],
+                contrasena=Codigo,
+                nombre=Nombre,
+                cargo="Lider Oficina de Practicas",
+            )
+            perfil.save()
+            creaAuxiliarOficina = True
+        else:
+            auxiliarExiste = True
+        return render(
+            request,
+             "Archivos/CrearAuxiliarOficina.html",
+            {
+                "creaAuxiliarOficina": creaAuxiliarOficina,
+                "auxiliarExiste": auxiliarExiste
+            },
+        )
 
 def asignarNuevoCoordinador(request):
     docentes = monitores.objects.filter()
@@ -371,118 +448,142 @@ def cargarArchivoEstudiantes(request):
 
 def cargarArchivoEstudiantesDos(request):
     existe = Estudiante.objects.exists()
+    semestre_no_existe = False
     if request.method == "GET":
         semestre = Semestres.objects.all()
         return render(request, "Archivos/CargaEstudiantesDos.html", {"semestre": semestre})
     else:
         try:
-            actualizados = 0
-            excel_file = request.FILES["excel_file"]
-            wb = openpyxl.load_workbook(excel_file)
-            worksheet = wb["ING SISTEMAS 2023 2"]
-            excel_data = []
+            periodo = request.POST.get("file_type")
+            semestre_seleccionado = Semestres.objects.get(id=periodo)
+            print(len(Estudiante.objects.filter(periodo_lectivo=semestre_seleccionado.nombre)))
+            if len(Estudiante.objects.filter(periodo_lectivo=semestre_seleccionado.nombre)) != 0:
+                actualizados = 0
+                excel_file = request.FILES["excel_file"]
+                wb = openpyxl.load_workbook(excel_file)
+                worksheet = wb["ING SISTEMAS 2023 2"]
+                excel_data = []
+                
+                for row in worksheet.iter_rows(min_row=4, max_col=28):
+                    row_data = []
+                    for cell in row:
+                        if str(cell.value) != "None":
+                            row_data.append(str(cell.value))
+                    if (
+                        len(row_data) >= 7
+                    ):  # Verificar si hay suficientes elementos en la lista
+                        excel_data.append(row_data)
 
-            for row in worksheet.iter_rows(min_row=4, max_col=28):
-                row_data = []
-                for cell in row:
-                    if str(cell.value) != "None":
-                        row_data.append(str(cell.value))
-                if (
-                    len(row_data) >= 7
-                ):  # Verificar si hay suficientes elementos en la lista
-                    excel_data.append(row_data)
+                for row in excel_data:
+                    # Verifica si la fila tiene suficientes elementos para procesar
+                    if (
+                        len(row) >= 28
+                    ):  # Ajusta el número según la cantidad de columnas en tu Excel
+                        # Crear un objeto Estudiante
 
-            for row in excel_data:
-                # Verifica si la fila tiene suficientes elementos para procesar
-                if (
-                    len(row) >= 28
-                ):  # Ajusta el número según la cantidad de columnas en tu Excel
-                    # Crear un objeto Estudiante
+                        plan_estudios = Plan_estudios(jornada=row[12])
+                        plan_estudios.save()
 
-                    plan_estudios = Plan_estudios(jornada=row[12])
-                    plan_estudios.save()
-
-                    if Estudiante.objects.filter(codigo=row[7]):
-                        print("estudiante ya existe")
-                        estudiante = Estudiante.objects.filter(codigo=row[7])[0]
-                        Estudiante.objects.filter(codigo=row[7]).update(
-                            nombre=row[5],
-                            apellidos=row[6],
-                            cedula=row[8],
-                            celular=row[9],
-                        )
-
-                        estudiante = Estudiante(
-                            programa=row[11],
-                            codigo=row[7],
-                            email_institucional=row[10],
-                            email_personal=row[10],
-                            telefono=row[9],
-                            nombre=row[5],
-                            apellidos=row[6],
-                            cedula=row[8],
-                            celular=row[9],
-                            periodo_lectivo=row[1]
-                            # Ajusta esto para el campo plan_estudios
-                        )
-                        actualizados += 1
-                        estudiante.save()
-
-                        # Crear un objeto Aspirantes relacionado con el estudiante
-                        aspirantes = Aspirantes(
-                            periodo_practica=row[1],
-                            aprobación_Programa=row[2],
-                            matriculado_Academica_y_Financieramente=row[4],
-                            inscripcion=row[13],
-                            curso_induccion_y_rl=row[14],
-                            ruta_preparacion_vida_laboral=row[15],
-                            envio_hv=row[16],
-                            titulo_tecnico_o_tecnologo=row[17],
-                            codigo_estudiante=estudiante,
-                        )
-                        aspirantes.save()
-
-                        if "Aplaza" in row[1]:
-                            periodo_aplazado = row[1].split("Aplaza ")[1]
-                            periodo_aplazado = periodo_aplazado[:-1]
-                            estudiante.periodo_lectivo = periodo_aplazado
-                            print(
-                                "Periodo despues de aplazar: "
-                                + row[1].split("Aplaza ")[1]
+                        if Estudiante.objects.filter(codigo=row[7]):
+                            print("estudiante ya existe")
+                            estudiante = Estudiante.objects.filter(codigo=row[7])[0]
+                            Estudiante.objects.filter(codigo=row[7]).update(
+                                nombre=row[5],
+                                apellidos=row[6],
+                                cedula=row[8],
+                                celular=row[9],
                             )
-                        elif row[1] == "NO APROBADO":
-                            estudiante.periodo_lectivo = "suspendido"
 
-                        estudiante.save()
+                            estudiante = Estudiante(
+                                programa=row[11],
+                                codigo=row[7],
+                                email_institucional=row[10],
+                                email_personal=row[10],
+                                telefono=row[9],
+                                nombre=row[5],
+                                apellidos=row[6],
+                                cedula=row[8],
+                                celular=row[9],
+                                periodo_lectivo=row[1]
+                                # Ajusta esto para el campo plan_estudios
+                            )
+                            actualizados += 1
+                            estudiante.save()
 
-                        # Crear un objeto Contrato relacionado con el estudiante
-                        print("entro If")
-                        contrato = Contrato(
-                            tipo_Contrato=row[21],
-                            fecha_Inicio=row[22],
-                            fecha_Final=row[23],
-                            encargado_Proceso_Seleccion=row[24],
-                            datos_Tutor_O_Jefe_Directivo=row[25],
-                            documentos_Pendientes=row[26],
-                            sector=row[27],
-                        )
-                        contrato.save()
+                            # Crear un objeto Aspirantes relacionado con el estudiante
+                            aspirantes = Aspirantes(
+                                periodo_practica=row[1],
+                                aprobación_Programa=row[2],
+                                matriculado_Academica_y_Financieramente=row[4],
+                                inscripcion=row[13],
+                                curso_induccion_y_rl=row[14],
+                                ruta_preparacion_vida_laboral=row[15],
+                                envio_hv=row[16],
+                                titulo_tecnico_o_tecnologo=row[17],
+                                codigo_estudiante=estudiante,
+                            )
+                            aspirantes.save()
 
-                        # Crear un objeto Estado_Practica relacionado con el estudiante, Aspirantes y Contrato
-                        estado_practica = Estado_Practica(
-                            codigo_estudiante=estudiante,
-                            practica_Donde_Labora_EmpresaFliar_Emprendim_Otro=row[18],
-                            estado_ubicación=row[19],
-                            comentarios=row[20],
-                            item=aspirantes,
-                            id_contrato=contrato,
-                        )
-                        estado_practica.save()
+                            if "Aplaza" in row[1]:
+                                periodo_aplazado = row[1].split("Aplaza ")[1]
+                                periodo_aplazado = periodo_aplazado[:-1]
+                                estudiante.periodo_lectivo = periodo_aplazado
+                                print(
+                                    "Periodo despues de aplazar: "
+                                    + row[1].split("Aplaza ")[1]
+                                )
+                            elif row[1] == "NO APROBADO":
+                                estudiante.periodo_lectivo = "suspendido"
 
+                            estudiante.save()
+
+                            # Crear un objeto Contrato relacionado con el estudiante
+                            print("entro If")
+                            contrato = Contrato(
+                                tipo_Contrato=row[21],
+                                fecha_Inicio=row[22],
+                                fecha_Final=row[23],
+                                encargado_Proceso_Seleccion=row[24],
+                                datos_Tutor_O_Jefe_Directivo=row[25],
+                                documentos_Pendientes=row[26],
+                                sector=row[27],
+                            )
+                            contrato.save()
+
+                            # Crear un objeto Estado_Practica relacionado con el estudiante, Aspirantes y Contrato
+                            estado_practica = Estado_Practica(
+                                codigo_estudiante=estudiante,
+                                practica_Donde_Labora_EmpresaFliar_Emprendim_Otro=row[18],
+                                estado_ubicación=row[19],
+                                comentarios=row[20],
+                                item=aspirantes,
+                                id_contrato=contrato,
+                            )
+                            estado_practica.save()
+                            return render(
+                                request,
+                                "Archivos/CargaEstudiantesDos.html",
+                                {
+                                    "semestre_no_existe": semestre_no_existe,
+                                    "excel_data": excel_data,
+                                    "existe": existe,
+                                    "actualizados": actualizados,
+                                },
+                            )
+            else:
+              semestre_no_existe = True
+              return render(
+                request,
+                "Archivos/CargaEstudiantesDos.html",
+                {
+                    "semestre_no_existe": semestre_no_existe
+                },
+            )  
             return render(
                 request,
                 "Archivos/CargaEstudiantesDos.html",
                 {
+                    "semestre_no_existe": semestre_no_existe,
                     "excel_data": excel_data,
                     "existe": existe,
                     "actualizados": actualizados,
